7db62e33a2389f316654f34cdb9d86e44ceb4408
hakim <hakim.amraoui@outlook.fr>
2021-02-09 16:17:32 +0100

    First Commit, Parties 1, 2

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,178 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences CYCLEVIEPREFS;
+    String INFOSECONDACTIVITY;
+
+
+    /**
+     * Exécuté chaque fois que l'utilisateur clique sur l'icône de l'application pour une première fois.
+     * <p>
+     * La fonction onCreate() est suivie d'un onStart().
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        popUp("onCreate()");
+
+        Button btnQuitter = (Button) findViewById(R.id.btnQuitter);
+        btnQuitter.setOnClickListener(btnQuitterOnClickListener);
+
+        Button btnEnvoyer = (Button) findViewById(R.id.btnEnvoyer);
+        btnEnvoyer.setOnClickListener(btnEnvoyerOnClickListener);
+
+        Button btnpAct2 = (Button) findViewById(R.id.btnAct2);
+        btnpAct2.setOnClickListener(btnAct2OnClickListener);
+
+    }
+
+    /**
+     * =============================================================
+     * Exécuté que l'activité arrêtée via un "stop" redémarre.
+     * <p>
+     * La fonction onRestart() est suivie de la fonction onStart().
+     */
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        popUp("onRestart()");
+    }
+
+    /**
+     * ==============================================================
+     * Exécuté lorsque l'activité devient visible à l'utilisateur.
+     * <p>
+     * La fonction onStart() est suivie de la fonction onResume().
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        popUp("onStart()");
+    }
+
+    /**
+     * ==============================================================
+     * Exécutée à chaque passage en premier plan de l'activité.
+     * Ou bien, si l'activité passe à nouveau en premier
+     * (si une autre activité était passée en premier plan entre temps).
+     * <p>
+     * La fonction onResume() est suivie de l'exécution de l'activité.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        popUp("onResume()");
+        CYCLEVIEPREFS = getSharedPreferences("cycle_vie_prefs", Context.MODE_PRIVATE);
+        setTxTValeur(CYCLEVIEPREFS.getString("valeur", ""));
+
+    }
+
+    public void setTxTValeur(String valeur) {
+        EditText zoneValeur = (EditText) findViewById(R.id.editTxtValeur);
+        zoneValeur.setText(valeur);
+    }
+
+
+    /**
+     * =============================================================
+     * La fonction onPause() est suivie :
+     * - d'un onResume() si l'activité passe à nouveau en premier plan
+     * - d'un onStop() si elle devient invisible à l'utilisateur
+     * <p>
+     * L'exécution de la fonction onPause() doit être rapide,
+     * car la prochaine activité ne démarrera pas tant que l'exécution
+     * de la fonction onPause() n'est pas terminée.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isFinishing()) {
+            popUp("onPause, l'utilisateur à demandé la fermeture via un finish()");
+        } else {
+            popUp("onPause, l'utilisateur n'a pas demandé la fermeture via un finish()");
+        }
+        SharedPreferences settings = getSharedPreferences("cycle_vie_prefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("valeur", getTxtValeur());
+        editor.commit();
+
+    }
+
+    /**
+     * ==============================================================
+     * La fonction onStop() est exécutée :
+     * - lorsque l'activité n'est plus en premier plan
+     * - ou bien lorsque l'activité va être détruite
+     * <p>
+     * Cette fonction est suivie :
+     * - de la fonction onRestart() si l'activité passe à nouveau en premier plan
+     * - de la fonction onDestroy() lorsque l'activité se termine
+     * ou bien lorsque le système décide de l'arrêter
+     */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        popUp("onStop()");
+    }
+
+    /**
+     * =============================================================
+     * Cette fonction est exécutée lorsque l'activité se termine ou bien lorsque
+     * le système décide de l'arrêter.
+     * <p>
+     * La fonction onCreate() devra à nouveau être exécutée pour obtenir à nouveau l'activité.
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        popUp("onDestroy()");
+    }
+
+    //=================================================================
+    View.OnClickListener btnQuitterOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            finish();
+        }
+    };
+
+    View.OnClickListener btnAct2OnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(v.getContext(), SecondActivity.class);
+            startActivity(intent);
+        }
+    };
+
+
+
+    View.OnClickListener btnEnvoyerOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            popUp("valeur saisie = " + getTxtValeur());
+            INFOSECONDACTIVITY = getTxtValeur();
+        }
+    };
+
+    public String getTxtValeur() {
+        EditText zoneValeur = (EditText) findViewById(R.id.editTxtValeur);
+        return zoneValeur.getText().toString();
+    }
+
+    public void popUp(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+}
